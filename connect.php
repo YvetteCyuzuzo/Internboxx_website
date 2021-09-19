633d14705cb3e075277046af79b0a8346fba3a3b
mamane19 <b.moussaama@alustudent.com>
2021-09-19 14:12:37 +0200

    fix: db connection

--- connect.php
+++ connect.php
@@ -1,11 +1,15 @@
 
     <?php
-    $conn = mysqli_connect("localhost", "root", "", "internships");
 
-    if ($conn === false) {
-        die("ERROR: Could not connect."
-            . mysqli_connect_error());
-    }
+    require("config/config.php");
+    require("config/db.php");
+
+    // $conn = mysqli_connect("localhost", "root", "", "internships");
+
+    // if ($conn === false) {
+    //     die("ERROR: Could not connect."
+    //         . mysqli_connect_error());
+    // }
 
     $degree = $_POST['degree'];
     $interest = $_POST['interest'];
@@ -17,14 +21,17 @@
     // database connection
 
     $sql = "INSERT INTO registration VALUES ('$degree','$interest','$type','$nature')";
+    $sql = "INSERT INTO user VALUES ('$degree','$interest','$type','$nature')";
+    $result = mysqli_query($conn, $sql);
+    header("Location:https://internboxx.herokuapp.com/patience.html");
 
-    if (mysqli_query($conn, $sql)) {
-        include 'patience.html';
-    } else {
-        echo "ERROR: Hush! Sorry $sql. "
-            . mysqli_error($conn);
-    }
-    mysqli_close($conn);
+    // if (mysqli_query($conn, $sql)) {
+    //     include 'patience.html';
+    // } else {
+    //     echo "ERROR: Hush! Sorry $sql. "
+    //         . mysqli_error($conn);
+    // }
+    // mysqli_close($conn);
 
 
     ?>
